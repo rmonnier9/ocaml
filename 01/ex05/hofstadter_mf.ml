e2db35e92ab5647cb1670ed117fca6485859a8aa
rmonnier <monnier.robin@gmail.com>
2019-01-08 23:32:12 +0100

    01 ex05

--- 01/ex05/hofstadter_mf.ml
+++ 01/ex05/hofstadter_mf.ml
@@ -10,18 +10,24 @@
 (*                                                                            *)
 (* ************************************************************************** *)
 
-let hfs_f n =
-    if n < 0
-    then (-1)
+let rec hfs_f n =
+if n < 0
+then (-1)
+else if n = 0
+then 1
+else (n - hfs_m (hfs_f (n - 1)))
 
-let hfs_f m =
-    if n < 0
-    then (-1)
+and hfs_m n =
+if n < 0
+then (-1)
+else if n = 0
+then 0
+else (n - hfs_f (hfs_m (n - 1)))
 
 let main () =
-    print_endline (string_of_int (fibonacci (-42)));
-    print_endline (string_of_int (fibonacci 1));
-    print_endline (string_of_int (fibonacci 3));
-    print_endline (string_of_int (fibonacci 6))
+    print_endline (string_of_int (hfs_m 0));
+    print_endline (string_of_int (hfs_f 0));
+    print_endline (string_of_int (hfs_m 4));
+    print_endline (string_of_int (hfs_f 4))
 
 let () = main ()
